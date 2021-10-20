07a0c169931603448535019c0b4decdf946e3dd6
jsleemaster <roeowo123@naver.com>
2021-10-20 23:44:26 +0900

    ios 실행실패

--- RNstudy/App.js
+++ RNstudy/App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
+import type { Node } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,9 +26,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
+const Section = ({ children, title }): Node => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
+    // ??
     <View style={styles.sectionContainer}>
       <Text
         style={[
