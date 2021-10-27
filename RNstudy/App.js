b36ff2f08c60cae6130904c6b5187177d552a4b9
jsleemaster <roeowo123@naver.com>
2021-10-27 23:35:20 +0900

    justifycontent

--- RNstudy/App.js
+++ RNstudy/App.js
@@ -15,8 +15,8 @@ import {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row'
-    // justifyContent: 'center',
+    flexDirection: 'column',
+    justifyContent: 'center', //flex-end , flex-start, center , space-between, space-around, space-evenly
     // alignItems: 'center'
   },
   hello: {
@@ -54,9 +54,9 @@ const App = () => {
       {/* 상위 flex 속해 있는 하위 flex는 태그 갯수에 따라 차지하는 비율의 최대가 정해지고 
           입력되는 값에 따라 비율을 정할 수 있다.
       */}
-      <View style={{ flex: 1, backgroundColor: 'red' }}></View>
-      <View style={{ flex: 2, backgroundColor: 'blue' }}></View>
-      <View style={{ flex: 3, backgroundColor: 'orange' }}></View>
+      <View style={{ flex: 1, backgroundColor: 'red', width: 50, height: 50 }}></View>
+      <View style={{ flex: 2, backgroundColor: 'blue', width: 100, height: 100 }}></View>
+      <View style={{ flex: 3, backgroundColor: 'orange', width: 150, height: 150 }}></View>
     </SafeAreaView>
   );
 };
