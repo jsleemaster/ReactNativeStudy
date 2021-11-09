a5079da4ee6f91e48f8420daa5edd6de57ff31d2
jsleemaster <roeowo123@naver.com>
2021-11-09 21:46:04 +0900

    useState

--- RNstudy/Components/Hook.js
+++ RNstudy/Components/Hook.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -15,13 +15,18 @@ import {
     Button
 } from 'react-native';
 const Hook = () => {
+    const [name, setName] = useState('smlee')
+
     return (
-        <View styles={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.flex}>
             <Text>Hooks</Text>
+            <Text>{name}</Text>
         </View>
     )
 }
 const styles = StyleSheet.create({
-
+    flex: {
+        flex: 1, alignItems: 'center', justifyContent: 'center'
+    }
 });
 export default Hook;
