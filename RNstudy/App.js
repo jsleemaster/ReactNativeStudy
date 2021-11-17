891b702fcb36ab92b60403a49852800be2ab262b
jsleemaster <roeowo123@naver.com>
2021-11-17 21:24:54 +0900

    vector-icons

--- RNstudy/App.js
+++ RNstudy/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import HookScreen from './Components/Hook'
 
 const Stack = createNativeStackNavigator();
@@ -201,7 +202,28 @@ const App = () => {
         /> */}
       {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
       {/* </Stack.Navigator> */}
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
